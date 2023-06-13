83f7fbf14898547fe7ad90ee88ab1f5d2c2ac6b4
Kamil Bartczak <kamil.bartczak@infermedica.com>
2023-06-13 11:10:02 +0200

    improve dispatch typing 1/2

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,48 @@
 import { reactive, ref } from "vue";
 
-export function createMachine<T extends {}, K extends any[]>(schema: {
-  state: () => T;
-  actions: {
-    [state: string]: {
-      onEnter?: (this: {
-        state: T;
-        changeState: (state: string) => void;
-        dispatch: (name: string, ...args: K) => void;
-      }) => any;
-      onLeave?: (this: {
-        state: T;
-        changeState: (state: string) => void;
-        dispatch: (name: string, ...args: K) => void;
-      }) => any;
-      [action: string]:
-        | ((
-            this: {
-              state: T;
-              changeState: (state: string) => void;
-              dispatch: (name: string, ...args: K) => void;
-            },
-            ...args: K
-          ) => any)
-        | undefined;
-    };
+type KeysOfUnion<T> = T extends T ? keyof T : never;
+
+type MachineActions<T> = {
+  [state: string]: {
+    onEnter?: (this: MachineContext<T>) => any;
+    onLeave?: (this: MachineContext<T>) => any;
+    [action: string]:
+      | ((this: MachineContext<T>, ...args: any[]) => any)
+      | undefined;
   };
-}) {
+};
+
+type MachineContext<T> = {
+  state: T;
+  changeState: (state: string) => void;
+  dispatch: (name: string, ...args: any[]) => void;
+};
+
+export function createMachine<
+  T extends {},
+  A extends MachineActions<T>
+>(schema: { state: () => T; actions: A }) {
   const currentState = ref("");
   const reactiveState = reactive({ ...schema.state() }) as T;
 
-  function changeState(state: string) {
+  type States = keyof A;
+
+  function changeState(state: States) {
     const thisCtx = { state: reactiveState, dispatch, changeState };
-    schema.actions[currentState.value]?.onLeave?.call(thisCtx);
-    currentState.value = state;
-    schema.actions[state]?.onEnter?.call(thisCtx);
+    schema.actions[currentState.value]?.onLeave?.call(thisCtx as any);
+    currentState.value = state as string;
+    schema.actions[state]?.onEnter?.call(thisCtx as any);
   }
 
-  function dispatch(actionName: string, ...args: K) {
-    const action = schema.actions[currentState.value][actionName];
+  function dispatch<Action extends KeysOfUnion<A[States]>>(
+    actionName: Action extends string ? Action : never,
+    ...args: any[]
+  ) {
+    const action = schema.actions[currentState.value][actionName as string];
     if (!action) throw new Error("Forbidden action!");
 
     return action.call(
-      { state: reactiveState, dispatch, changeState },
+      { state: reactiveState, dispatch: dispatch as any, changeState },
       ...args
     );
   }
