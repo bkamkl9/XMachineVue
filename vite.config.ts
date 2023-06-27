b1d8cda418ba01e020b7b98927967c53c3e46682
Kamil Bartczak <kamil.bartczak@infermedica.com>
2023-06-27 22:19:06 +0200

    feat: improve docs and type system

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,29 @@
-import { defineConfig } from "vite";
-import { resolve } from "node:path";
-import typescript from "@rollup/plugin-typescript";
-import { typescriptPaths } from "rollup-plugin-typescript-paths";
+import { defineConfig } from 'vite'
+import { resolve } from 'node:path'
+import typescript from '@rollup/plugin-typescript'
 
 export default defineConfig({
   plugins: [],
   build: {
     minify: true,
     lib: {
-      entry: resolve(__dirname, "./src/index.ts"),
-      fileName: "index",
-      formats: ["es", "cjs"],
+      entry: resolve(__dirname, './src/index.ts'),
+      fileName: 'index',
+      formats: ['es', 'cjs'],
     },
     rollupOptions: {
-      external: ["vue"],
+      external: ['vue'],
       output: {
-        globals: { vue: "Vue" },
-        exports: "named",
+        globals: { vue: 'Vue' },
+        exports: 'named',
       },
       plugins: [
         typescript({
           sourceMap: false,
           declaration: true,
-          outDir: "dist",
-        }),
-        typescriptPaths({
-          preserveExtensions: true,
+          outDir: 'dist',
         }),
       ],
     },
   },
-});
+})
