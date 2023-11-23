107eac62ca3cc877265bc961b35737908e83e1d6
Kamil Bartczak <kamil.bartczak@infermedica.com>
2023-11-23 14:49:16 +0100

    storing state and initial state object in StateController

--- src/controllers/state.controller.ts
+++ src/controllers/state.controller.ts
@@ -1,9 +1,14 @@
 import { InstanceManager } from '../managers/instance.manager'
+import { Ref, ref } from 'vue'
 
 export class StateController {
   InstanceManager: InstanceManager
+  InitialState: string
+  CurrentState: Ref<string>
 
   constructor(InstanceManager: InstanceManager) {
     this.InstanceManager = InstanceManager
+    this.InitialState = InstanceManager.machineSchema.initial
+    this.CurrentState = ref(InstanceManager.machineSchema.initial)
   }
 }
