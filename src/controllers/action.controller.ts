9a274064b7ab0e294939f0c3e949bf11d2bc0605
Kamil Bartczak <kamil.bartczak@infermedica.com>
2023-11-23 14:47:21 +0100

    store states object of actions

--- src/controllers/action.controller.ts
+++ src/controllers/action.controller.ts
@@ -1,9 +1,26 @@
 import { InstanceManager } from '../managers/instance.manager'
+import { AnyFunction } from '../types/helper.types'
 
 export class ActionController {
   InstanceManager: InstanceManager
+  ActionsObject: XMACHINEVUE.MachineSchema['states']
 
   constructor(InstanceManager: InstanceManager) {
     this.InstanceManager = InstanceManager
+    this.ActionsObject = this.createObjectOfActions()
+  }
+
+  private createObjectOfActions() {
+    let schema = structuredClone(this.InstanceManager.machineSchema.states)
+
+    for (const state in schema) {
+      for (const action in schema[state]) {
+        // TODO: create wrapper function
+        // TODO: assign context object
+        schema[state][action] = schema[state][action]
+      }
+    }
+
+    return schema
   }
 }
