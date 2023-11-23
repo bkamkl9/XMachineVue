b13b0d372498cc738a75e3b656b75d2467bdab86
Kamil Bartczak <kamil.bartczak@infermedica.com>
2023-11-23 14:56:28 +0100

    add state guards

--- src/controllers/action.controller.ts
+++ src/controllers/action.controller.ts
@@ -3,24 +3,34 @@ import { AnyFunction } from '../types/helper.types'
 
 export class ActionController {
   InstanceManager: InstanceManager
-  ActionsObject: XMACHINEVUE.MachineSchema['states']
+  ActionsObject: XMACHINEVUE.ActionObjectSchema
 
   constructor(InstanceManager: InstanceManager) {
     this.InstanceManager = InstanceManager
     this.ActionsObject = this.createObjectOfActions()
   }
 
+  private ThrowWrongStateErr(actionName: string, expectedState: string) {
+    const currentState = this.InstanceManager.StateController.CurrentState.value
+    throw new Error(`Action ${actionName} cannot be executed in ${currentState}. Expected state: ${expectedState}`)
+  }
+
+  private decorateWithStateGuard(method: AnyFunction, actionName: string, expectedState: string) {
+    return (...args: any[]) => {
+      const currentState = this.InstanceManager.StateController.CurrentState.value
+      if (currentState !== expectedState) this.ThrowWrongStateErr(actionName, expectedState)
+      return method(...args)
+    }
+  }
+
   private createObjectOfActions() {
     let schema = structuredClone(this.InstanceManager.machineSchema.states)
-
     for (const state in schema) {
       for (const action in schema[state]) {
-        // TODO: create wrapper function
-        // TODO: assign context object
-        schema[state][action] = schema[state][action]
+        const method = schema[state][action]
+        schema[state][action] = this.decorateWithStateGuard(method, action, state)
       }
     }
-
     return schema
   }
 }
