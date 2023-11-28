29b5f86cdc6826ebdc46b3a7fb4f99315c971042
Kamil Bartczak <kamil.bartczak@infermedica.com>
2023-11-28 20:05:17 +0100

    Improve typescript autocomplete

--- src/core/types/helper.types.ts
+++ src/core/types/helper.types.ts
@@ -1,3 +1,13 @@
-export type AnyFunction = (...args: any[]) => any
+export type AnyFunction = (this: any, ...args: any[]) => any
 
 export type AnyObject = Record<any, any>
+
+export type RemoveThis<S> = {
+  [state in keyof S]: {
+    [action in keyof Omit<S[state], '$onEnter' | '$onLeave'>]: S[state][action] extends (
+      ...args: infer Args
+    ) => infer ReturnValue
+      ? (this: any, ...args: Args) => ReturnValue
+      : never
+  }
+}
